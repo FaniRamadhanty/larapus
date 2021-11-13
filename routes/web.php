537815ba4d677970e6071cd438c6bf08778ecd22
FaniRamadhanty <fani_173@smkassalaambandung.sch.id>
2021-11-13 11:22:16 +0700

    CRUD

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthorController;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,23 +19,25 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-
-
-//Route Tester Admin Template
-Route::get('test-admin', function (){
-    return view('layouts.admin');
+// route tester admin template
+Route::get('tes-admin',function(){
+return view('layouts.admin');
 });
 
 Auth::routes();
-    Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-    // Admin Route
-    Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function() { 
-    Route::group(['prefix' => 'admin', 'middleware'=>['auth','role:admin']], function () {
-        Route::get('/', function (){
-            return view('admin.index');
-        });
-    });
-    });
+
+Route::group(
+    ['prefix'=>'admin', 'middleware'=>['auth','role:admin']], function (){
+   Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+});
+
+Route::group(
+    ['prefix'=>'user', 'middleware'=>['auth','role:member']], function (){
+   Route::get('/sample', [App\Http\Controllers\HomeController::class, 'index2'])->name('home');
+});
+
+Route::resource('author', AuthorController::class);
+Route::resource('book', BookController::class);
+
+
